bbc3995626969e671edc15f6b34ef2aea7ba10bc
tshi04 <tshi@vt.edu>
2020-12-16 20:34:54 +0000

    update

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
-### Document Level Multi-Ascept Sentiment Classification (DMSC)
-# from DMSC.mtAttn.main import *
-# from DMSC.mtAttnFE.main import *
+# Document Level Multi-Ascept Sentiment Classification (DMSC)
 # from DMSC.mtCNN.main import *
-# from DMSC.mtAttnDA.main import *
-# from DMSC.FEDA.main import *
 # from DMSC.mtRNN.main import *
+# from DMSC.mtAttn.main import *
+# from DMSC.mtAttnDA.main import *
+# from DMSC.mtAttnFE.main import *
+# from DMSC.FEDA.main import *
 # from DMSC.mtBertAttn.main import *
 ### DMSC + Uncertainty
 # from DMSCFEDA.FEDA.main import *
 # from DMSCFEDA.FEDAR.main import *
-
